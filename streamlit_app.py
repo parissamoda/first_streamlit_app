3fbe053bec75ab35c644029b4009578bec35e1c5
parissamoda <59227368+parissamoda@users.noreply.github.com>
2023-10-12 11:14:20 -0600

    Update streamlit_app.py
    new title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('Parissa's First App :D')
+streamlit.title('Parissa's First App')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale,Spinach & Rocket Smoothie')
