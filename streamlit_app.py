ce0882041f72429a0b40f69084d13419373a9ced
parissamoda <59227368+parissamoda@users.noreply.github.com>
2023-10-18 16:02:38 -0600

    snowflake connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 import pandas
-
+import snowflake.connector
 streamlit.title('Whatever you want')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
