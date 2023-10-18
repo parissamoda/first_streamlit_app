ce67f1aeda602826ca87ad43326b7491a44026a0
parissamoda <59227368+parissamoda@users.noreply.github.com>
2023-10-18 16:07:05 -0600

    new title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 import snowflake.connector
-streamlit.title('Whatever you want')
+streamlit.title('My Mom's New Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale,Spinach & Rocket Smoothie')
