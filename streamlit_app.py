bc6847d5469cf15496b92a1b4e6104c99e03f5b4
parissamoda <59227368+parissamoda@users.noreply.github.com>
2023-10-19 18:10:39 -0600

    exception

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,17 +19,19 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-
+try:
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-
+if not fruit_choice:
+  streamlit.error("Please select a fruit to get information.")
+else:
+# streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
-
+except URLError as e:
+streamlit.error()
 # connecting snowflake
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
@@ -43,6 +45,6 @@ streamlit.dataframe(my_data_rows)
 ##### add a fruit
 fruit_to_add = streamlit.text_input("What fruit would you like to add?")
 streamlit.write('Thanks for adding ', fruit_to_add)
-my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit') ")
+# my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit') ")
 # insert_query = f"INSERT INTO pc_rivery_db.public.fruit_load_list (FRUIT_NAME) VALUES ('{fruit_to_add}')"
 # my_cur.execute(insert_query)
